4846c3b102d44b718f0009ebf90203ad6153f179
Aliaksei Paliachynski <aliaksei.paliachynski@startmatter.com>
2023-04-20 22:31:40 +0300

    added queue and dequeue

---
+++ src/Homework_4/Queue/index.ts
@@ -0,0 +1,22 @@
+import { LinkedList } from "../../Homework_3";
+
+class Queue<T> {
+    #list: LinkedList<T>;
+    constructor() {
+        this.#list = new LinkedList();
+    }
+
+    get head() {
+        return this.#list.first!?.value
+    }
+
+    push(value: T) {
+        this.#list.addLast(value)
+    }
+
+    shift() {
+        return this.#list.removeFirst()
+    }
+
+}
+export { Queue };
