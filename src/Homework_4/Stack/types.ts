039ea26362e4a2ec63b385675db255779b4de082
Aliaksei Paliachynski <aliaksei.paliachynski@startmatter.com>
2023-04-24 14:05:47 +0300

    added stack

---
+++ src/Homework_4/Stack/types.ts
@@ -0,0 +1,23 @@
+type TypedArrayConstructor =
+  | Int8ArrayConstructor
+  | Uint8ArrayConstructor
+  | Uint8ClampedArrayConstructor
+  | Int16ArrayConstructor
+  | Uint16ArrayConstructor
+  | Int32ArrayConstructor
+  | Uint32ArrayConstructor
+  | Float32ArrayConstructor
+  | Float64ArrayConstructor;
+
+type TypedArray =
+  | Int8Array
+  | Uint8Array
+  | Uint8ClampedArray
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array;
+
+export { TypedArrayConstructor, TypedArray };
